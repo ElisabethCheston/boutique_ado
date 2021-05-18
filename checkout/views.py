7c188424d3e5a75b139bd63eec39329e354727ab
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-05-18 15:10:52 +0000

    Added stripe elements.

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IsTFXGb1tfsAqedvH9qYMtwL4wF4Wxk9JM0Kfxx2CaAnSgHDzPA4oKvD5NdDKACIHLB9JKU9uOI9FayWIxI7LLU00r7lftTXz',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
